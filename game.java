69131884ee9be7c83be14ecf6d430461766b9878
Vaibhav <vaibhavkumawat21318@gmail.com>
2023-10-29 18:06:12 +0530

    variables

--- game.java
+++ game.java
@@ -3,10 +3,33 @@ import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
+import javax.swing.Timer;
 
 public class game extends JPanel{
     
-    private ImageIcon Titleimage;
+    // length of snake : 
+    private int[] xsnakelength = new int[750];
+    private int[] ysnakelength = new int[750];
+
+    // variables for movement directions :
+    private boolean right = false; 
+    private boolean left = false; 
+    private boolean up = false; 
+    private boolean down = false;
+
+
+    private ImageIcon Titleimage;   // title image variable
+    // Snake image variable for mouth: 
+    private ImageIcon rightmouth;
+    private ImageIcon leftmouth;
+    private ImageIcon upmouth;
+    private ImageIcon downmouth;
+    private ImageIcon snakeimage; //snake body variable 
+
+    //variable for Speed of snake :
+    private Timer timer;
+    private int delay=100; 
+
     public game(){}
 
     public void paint(Graphics g){
@@ -15,8 +38,8 @@ public class game extends JPanel{
         g.setColor(Color.WHITE);
         g.drawRect(25,10,850,55);
 
-        Titleimage = new ImageIcon("./assets/Title_snake.jpg");
-        Titleimage.paintIcon(this,g,45,11);
+        Titleimage = new ImageIcon("./assets/snaketitle.jpg");
+        Titleimage.paintIcon(this,g,26,11);
 
         //Border for game-play :
         g.setColor(Color.WHITE);
@@ -25,5 +48,7 @@ public class game extends JPanel{
         // set the background color of game-play area : 
         g.setColor(Color.BLACK);
         g.fillRect(26,76,849,572);
+
+        
     }
 }
