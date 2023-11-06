fdf91f9cbcb1cd7ce813a3f225acba5c10409919
Vaibhav <vaibhavkumawat21318@gmail.com>
2023-11-06 20:07:24 +0530

    full game

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 package snakegame;
 import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Main{
     public static void main(String[] args){
         // Frame setting amd naming : 
         JFrame frame = new JFrame("Snake game");  //Title set
-        frame.setBounds(10,10,905,700); //Set the boundary (Left margin , top margin , width , height)
+        frame.setBounds(10,19,905,700); //Set the boundary (Left margin , top margin , width , height)
         frame.setResizable(false);
-        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-
-        Panel G = new Panel();
-        G.setBackground(Color.DARK_GRAY);
-        frame.add(G);
+        Panel panel = new Panel();
+        panel.setBackground(Color.DARK_GRAY);
+        frame.add(panel);
+        frame.setVisible(true);
     }
 }
--- Panel.java
+++ Panel.java
@@ -1,30 +1,40 @@
 package snakegame;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
+import java.util.Random;
+import javax.swing.Timer;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
-import javax.swing.Timer;
+public class Panel extends JPanel implements ActionListener, KeyListener{
 
-public class Panel extends JPanel implements ActionListener,KeyListener{
+    //length of snake
+    private int[] snakexlength=new int[750];
+    private int[] snakeylength=new int[750];
+    private int lengthofsnake=3;  //length of body including head
 
-    // length of snake : 
-    private int[] snakexlength = new int[750];  // x coordinate
-    private int[] snakeylength = new int[750];  // y coordinate
-    private int lengthofsnake=3;  //body length including head
+    //set of coordinates predefined for the enemy
+    private int[] xPos={25,50,75,100,125,150,175,200,225,250,275,300,325,350,375,400,425,450,475,500,525,550,575,600,625,650,675,700,725,750,775,800,825,850};
+    private int[] yPos={75,100,125,150,175,200,225,250,275,300,325,350,375,400,425,450,475,500,525,550,575,600,625};
+
+    private Random random=new Random(); //to selecting random value for enemy coordinate
+    private int enemyX,enemyY;
+
+    //variables for movement directions :
+    private boolean left=false;
+    private boolean right=true;
+    private boolean up=false;
+    private boolean down=false;
+    private int moves=0;
     
-    // variables for movement directions :
-    private boolean right = true; 
-    private boolean left = false; 
-    private boolean up = false; 
-    private boolean down = false;
-    private int moves=0; //to check the moves
+    private int score=0; //score board and game over
+    private boolean gameOver=false;
+    private boolean pause=false;
 
-    
     //Image variable declare and provided link of image
     private ImageIcon snaketitle = new ImageIcon( getClass().getResource("snaketitle.jpg")); 
     private ImageIcon snakeimage = new ImageIcon( getClass().getResource("snakeimage.png")); 
@@ -32,50 +42,48 @@ public class Panel extends JPanel implements ActionListener,KeyListener{
     private ImageIcon upmouth = new ImageIcon( getClass().getResource("upmouth.png")); 
     private ImageIcon rightmouth = new ImageIcon( getClass().getResource("rightmouth.png")); 
     private ImageIcon leftmouth = new ImageIcon( getClass().getResource("leftmouth.png")); 
-    
-    //variable for Speed of snake :
-    private Timer timer;
-    private int delay=100; //100 milisecond
+    private ImageIcon enemy = new ImageIcon( getClass().getResource("enemy.png")); 
 
-    Panel(){
+    //speed of snake
+    private Timer timer;
+    private int delay=100;
+
+    Panel() {
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(true);
-        
-        timer = new Timer(delay,this);
-        timer.start();  //this timer class will call actionlistener method
+        timer=new Timer(delay,this);
+        timer.start();
+        newEnemy();
     }
 
+
+
     @Override
-    public void paint(Graphics g){
-        super.paint(g); 
-
-        //Border for title image :
-        g.setColor(Color.WHITE);
-        g.drawRect(24,10,851, 55);
-
-        //Border for game-play :
-        g.setColor(Color.WHITE);
-        g.drawRect(24,74,851,576);
-
-        snaketitle.paintIcon(this, g, 25, 11);  //setting the snake title image : 
+    public void paint(Graphics g) {
+        super.paint(g);
+        //for title :
+        g.setColor(Color.white);
+        g.drawRect(24, 10, 851, 53);
+        g.drawRect(24, 74, 851, 576);
         
-        //for the background rectangle : 
-        g.setColor(Color.BLACK);  //playground color 
+        //for playground :
+        snaketitle.paintIcon(this,g,25 ,11);
+        g.setColor(Color.BLACK);
         g.fillRect(25, 75, 850, 575);
 
-        // intial position for snake : 
+        //intial position of snake
         if(moves==0){
-            snakexlength[0]=100;  // mouth position at start
-            snakexlength[1]=75;  
-            snakexlength[2]=50; 
-        
-            snakeylength[0]=100;  // mouth position at start
-            snakeylength[1]=100;  
-            snakeylength[2]=100;  
-            moves++;
+            snakexlength[0]=100;
+            snakexlength[1]=75;
+            snakexlength[2]=50;
+
+            snakeylength[0]=100;
+            snakeylength[1]=100;
+            snakeylength[2]=100;
         }
 
+        //pasting the snake-body image 
         if(left){
             leftmouth.paintIcon(this,g,snakexlength[0],snakeylength[0]);
         }
@@ -88,20 +96,53 @@ public class Panel extends JPanel implements ActionListener,KeyListener{
         if(down){
             downmouth.paintIcon(this,g,snakexlength[0],snakeylength[0]);
         }
-
         for(int i=1;i<lengthofsnake;i++){
             snakeimage.paintIcon(this,g,snakexlength[i],snakeylength[i]);
         }
-        g.dispose();
+
+        //painting enemy ball : 
+        enemy.paintIcon(this,g,enemyX,enemyY);
+        g.drawString("Score : "+score, 750, 30);
+        g.drawString("Length : "+lengthofsnake, 750, 50);
+
+
+        if(gameOver){    //game over 
+            g.setColor(Color.white);
+            g.setFont(new Font("Arial",Font.BOLD,50));
+            g.drawString("Game Over!", 300, 300);
+
+            g.setFont(new Font("Arial",Font.PLAIN,20));
+            g.drawString("Press SPACE to Restart", 320, 350);
+
+            g.setColor(Color.white);
+            g.setFont(new Font("Arial",Font.PLAIN,14));
+        }
+
+        if(pause){   //Pause contain
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", Font.BOLD, 50));
+        g.drawString("Game Paused", 300, 300);
+
+        g.setFont(new Font("Arial", Font.PLAIN, 20));
+        g.drawString("Press ENTER to Continue", 320, 350);
+
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", Font.PLAIN, 14));
+        }
+        
+
+        
     }
 
     @Override
     public void actionPerformed(ActionEvent e){
+
+        if(!pause){
+        //logic for running of snake
         for(int i=lengthofsnake-1;i>0;i--){
             snakexlength[i]=snakexlength[i-1];
             snakeylength[i]=snakeylength[i-1];
         }
-
         if(left){
             snakexlength[0]=snakexlength[0]-25;
         }
@@ -112,58 +153,116 @@ public class Panel extends JPanel implements ActionListener,KeyListener{
             snakeylength[0]=snakeylength[0]-25;
         }
         if(down){
-            snakexlength[0]=snakexlength[0]+25;
+            snakeylength[0]=snakeylength[0]+25;
         }
 
-        //to keep the in bound and return from the parallel side
-        if(snakexlength[0]>850){
-            snakexlength[0]=25;
-        }
-        if(snakeylength[0]<25){
-            snakeylength[0]=850;
-        }
-        if(snakeylength[0]>625){
-            snakeylength[0]=75;
-        }
-        if(snakeylength[0]<75){
-            snakeylength[0]=625;
-        }
+        if(snakexlength[0]>850)snakexlength[0]=25;
+        if(snakexlength[0]<25)snakexlength[0]=850;
+
+        if(snakeylength[0]>625)snakeylength[0]=75;
+        if(snakeylength[0]<75)snakeylength[0]=625;
+
+        collideswithenemy();
+        collideswithbody();
 
         repaint();
+    }
+    }
+
+    @Override  //not of use but have to Override in order to implement KeyListener
+    public void keyTyped(KeyEvent e) {
+        
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode()==KeyEvent.VK_LEFT){
+        // getting values for the arrow - keys : 
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            restart();
+        } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            if (pause) { // Unpause the game
+                pause = false;
+                repaint(); // Remove the pause message
+            } else { // Pause the game
+                pause=true;
+                repaint();  //print the pause message
+            }
+        }
+        if(!pause){
+        if(e.getKeyCode()==KeyEvent.VK_LEFT && (!right)){
             left=true;
             right=false;
             up=false;
             down=false;
+            moves++;
         }
-        if(e.getKeyCode()==KeyEvent.VK_RIGHT){
+        if(e.getKeyCode()==KeyEvent.VK_RIGHT && (!left)){
             left=false;
             right=true;
             up=false;
             down=false;
+            moves++;
         }
-        if(e.getKeyCode()==KeyEvent.VK_UP){
+        if(e.getKeyCode()==KeyEvent.VK_UP && (!down)){
             left=false;
             right=false;
             up=true;
             down=false;
+            moves++;
         }
-        if(e.getKeyCode()==KeyEvent.VK_DOWN){
+        if(e.getKeyCode()==KeyEvent.VK_DOWN && (!up)){
             left=false;
             right=false;
             up=false;
             down=true;
+            moves++;
         }
     }
 
-    // these 2 methods are no in used but have to be implement 
-    @Override
-    public void keyTyped(KeyEvent e){}
+    }
 
-    @Override
+    @Override //not in use but have to be override 
     public void keyReleased(KeyEvent e){}
+
+    private void newEnemy() {
+        enemyX=xPos[random.nextInt(34)];
+        enemyY=yPos[random.nextInt(23)];
+
+        for(int i=lengthofsnake-1;i>=0;i--){
+            if(snakexlength[i]==enemyX && snakeylength[i]==enemyY){
+                newEnemy();
+            }
+        }
+    }
+
+    private void collideswithenemy() {
+        if(snakexlength[0]==enemyX && snakeylength[0]==enemyY){
+            newEnemy();
+            lengthofsnake++;
+            score++;
+        }
+    }
+
+    private void collideswithbody() {
+        for(int i=lengthofsnake-1;i>0;i--){
+            if(snakexlength[i]==snakexlength[0] && snakeylength[i]==snakeylength[0]){
+                timer.stop();
+                gameOver=true;
+            }
+        }
+    }
+
+    private void restart(){
+        gameOver=false;
+        moves=0;
+        score=0;
+        lengthofsnake=3;
+        left=false;
+        right=true;
+        up=false;
+        down=false;
+        timer.start();
+        newEnemy();
+    }
+    
 }
